144114f7678aa49ad742424876c46d19ae23986b
KiesheaH <59402191+KiesheaH@users.noreply.github.com>
2025-12-15 23:09:33 -0500

    Remove darken and lighten buttons
    Removed unfinished darken and lighten features.
    Known issue: gridlines toggle resets when grid size changes.

--- script.js
+++ script.js
@@ -7,23 +7,53 @@ const columnDisplay = document.querySelector(".column");
 const slider = document.querySelector(".slider");
 const gridLinesBtn = document.querySelector(".gridLines");
 const rainbowBtn = document.querySelector(".rainbow");
-const darkenBtn = document.querySelector(".darken");
-const lightenBtn = document.querySelector(".lighten");
 const eraserBtn = document.querySelector(".eraser");
 const clearBtn = document.querySelector(".clear");
 const container = document.querySelector(".container");
 
 // global variables
-let grid;
+let square;
 let size;
 let color;
+let style;
+
+let activeColor = "#ff8700";
+let activeFontColor = "#fff";
+
+let inactiveColor = "#fff";
+let inactiveFontColor = "#000";
 
 // boolean variables
 let isDrawing = false;
 
-// display grid size
+// create sketch area
+function createGrid(size) {
+  // clear container
+  container.textContent = "";
+
+  // determine size of the squares
+  const squareHeight = 700 / size;
+  const squareWidth = 700 / size;
+
+  // create grid using squares
+  for (let i = 0; i < size * size; i++) {
+    square = document.createElement("div");
+    square.className = "gridCells";
+    square.style.height = `${squareHeight}px`;
+    square.style.width = `${squareWidth}px`;
+    container.appendChild(square);
+  }
+}
+
+function colorSquare() {
+  this.style.backgroundColor = colorPicker.value;
+}
+
+// change grid size
 slider.addEventListener("input", (e) => {
   size = e.target.value;
+
+  container.textContent = "";
 
   // update row and height text
   rowDisplay.textContent = size;
@@ -31,71 +61,11 @@ slider.addEventListener("input", (e) => {
 
   // update grid container
   createGrid(size);
-
-  // color reset
-  container.style.backgroundColor = "#fff";
-  colorPicker.value = "#000";
-
-  // button resets
-  rainbowBtn.classList.remove("active");
-  rainbowBtn.style.backgroundColor = "#fff";
-  rainbowBtn.style.color = "#000";
-
-  return size;
+  reset(size);
+  // return size;
 });
 
-// change grid container by size
-function createGrid(size) {
-  // clear container
-  container.textContent = "";
-
-  // determine size of the grid cell
-  const gridCellHeight = 700 / size;
-  const gridCellWidth = 700 / size;
-
-  // create grid
-  for (let i = 0; i < size * size; i++) {
-    grid = document.createElement("div");
-    grid.className = "gridCells";
-    grid.style.height = `${gridCellHeight}px`;
-    grid.style.width = `${gridCellWidth}px`;
-    grid.classList.add("show_grid");
-    container.appendChild(grid);
-  }
-}
-
-// remove grid lines
-gridLinesBtn.addEventListener("click", function () {
-  let gridCells = document.querySelectorAll(".gridCells");
-  let gridLines = Array.from(gridCells);
-
-  for (let i = 0; i < gridLines.length; i++) {
-    gridLines[i].classList.toggle("show_grid");
-  }
-});
-
-// rainbow colors
-rainbowBtn.addEventListener("click", (e) => {
-  e.target.classList.toggle("active");
-  if (e.target.classList.contains("active")) {
-    e.target.style.backgroundColor = "#ff8700";
-    e.target.style.color = "#fff";
-  } else {
-    e.target.style.backgroundColor = "#fff";
-    e.target.style.color = "#000";
-  }
-});
-
-// darken colors
-darkenBtn.addEventListener("click", (e) => {
-  e.target.classList.toggle("active");
-});
-
-// lighten colors
-lightenBtn.addEventListener("click", (e) => {
-  e.target.classList.toggle("active");
-});
-
+// SKETCH AREA
 // use etch-a-sketch
 document.addEventListener("mousedown", () => {
   isDrawing = true;
@@ -119,4 +89,87 @@ container.addEventListener("mouseover", (e) => {
     let g = Math.floor(Math.random() * 255 + 1);
     e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
   }
+
+  // erase selected colors
+  if (eraserBtn.classList.contains("active")) {
+    e.target.style.backgroundColor = "#fff";
+  }
+});
+
+// FUNCTIONS
+// changeBtnColors
+function activeButton(e) {
+  e.target.classList.toggle("active");
+  if (e.target.classList.contains("active")) {
+    e.target.style.backgroundColor = activeColor;
+    e.target.style.color = activeFontColor;
+  } else {
+    e.target.style.backgroundColor = inactiveColor;
+    e.target.style.color = inactiveFontColor;
+  }
+}
+
+function inactive(btn) {
+  btn.style.backgroundColor = inactiveColor;
+  btn.style.color = inactiveFontColor;
+}
+
+function reset(size) {
+  // update row and height text
+  slider.value = size;
+  rowDisplay.textContent = size;
+  columnDisplay.textContent = size;
+
+  // update grid container
+  createGrid(size);
+
+  // color reset
+  container.style.backgroundColor = inactiveColor;
+  colorPicker.value = "rgb(0,0,0)";
+
+  // button resets
+  inactive(gridLinesBtn);
+  gridLinesBtn.textContent = "GRID LINES: OFF";
+  // gridLinesBtn.classList.add("active");
+
+  rainbowBtn.classList.remove("active");
+  inactive(rainbowBtn);
+
+  eraserBtn.classList.remove("active");
+  inactive(eraserBtn);
+}
+
+// BUTTONS
+// remove grid lines
+gridLinesBtn.addEventListener("click", (e) => {
+  let gridCells = document.querySelectorAll(".gridCells");
+  let gridLines = Array.from(gridCells);
+
+  for (let i = 0; i < gridLines.length; i++) {
+    gridLines[i].classList.toggle("show_grid");
+  }
+
+  if (!e.target.classList.contains("active")) {
+    e.target.textContent = "GRID LINES: ON";
+  }
+
+  if (e.target.classList.contains("active")) {
+    e.target.textContent = "GRID LINES: OFF";
+  }
+  activeButton(e);
+});
+
+// rainbow colors
+rainbowBtn.addEventListener("click", (e) => {
+  activeButton(e);
+});
+
+// erase colors
+eraserBtn.addEventListener("click", (e) => {
+  activeButton(e);
+});
+
+// clear all
+clearBtn.addEventListener("click", (e) => {
+  reset(1);
 });
